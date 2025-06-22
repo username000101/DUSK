9c4c351bafcee6451cb33db6cee621617b8a7d3e
username000101 <cxx.username000101@gmail.com>
2025-06-22 03:50:58 +0300

    Make and use default logger for making debug/trace messages

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+
 #include "Args.hpp"
 #include "Macros.hpp"
-
+#include "TerminateHandler.hpp"
 
 int main(int argc, char** argv) {
+    auto defcon = std::make_shared<spdlog::logger>("Default", spdlog::sinks_init_list{std::make_shared<spdlog::sinks::stdout_color_sink_mt>()});
+    spdlog::set_default_logger(defcon);
+#ifndef NDEBUG
+    spdlog::set_level(spdlog::level::debug);
+    spdlog::flush_on(spdlog::level::debug);
+#else
+    spdlog::set_level(spdlog::level::info);
+    spdlog::flush_on(spdlog::level::info);
+#endif
+
+    std::set_terminate(terminate_handler_f);
     std::cout << DUSK_LOGO << std::endl << std::endl;
     args::process_args(argc, argv);
     return 0;

a0873d912f671a4f9687d98cb8c15bc61468b51e
username000101 <cxx.username000101@gmail.com>
2025-06-22 03:53:26 +0300

    Add the globals::configuration variable for DUSK configuration

--- Utils/Globals.hpp
+++ Utils/Globals.hpp
@@ -2,6 +2,9 @@
 
 #include <cstdint>
 
+#include "Configuration.hpp"
+
 namespace globals {
     inline std::int64_t current_user;
+    inline std::shared_ptr<config::Configuration> configuration;
 }

1b4e68553e78168d85f18f52098217d6a8a63581
username000101 <cxx.username000101@gmail.com>
2025-06-23 23:05:45 +0300

    Fix the extracted_modpack_dir_ path

--- Modpack/src/ExtractModpack.cpp
+++ Modpack/src/ExtractModpack.cpp
@@ -4,6 +4,6 @@
 
 std::filesystem::path modpack::ModpackReader::extract_modpack(std::filesystem::path into) {
     libarchive::Archive archive(this->file_);
-    this->extracted_modpack_path_ = archive.extract(into);
+    this->extracted_modpack_path_ = archive.extract(into) / this->file_.stem();
     return this->extracted_modpack_path_;
 }

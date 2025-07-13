36a257f35c6392a934ccae120ec84b7baea968dc
username000101 <cxx.username000101@gmail.com>
2025-07-13 16:33:46 +0300

    Implemented the InitUser function

---
+++ Filesystem/src/InitUser.cpp
@@ -0,0 +1,30 @@
+#include "FS.hpp"
+
+#include <fstream>
+
+#include <spdlog/spdlog.h>
+
+#include "Globals.hpp"
+
+void filesystem::init_user() {
+    if (globals::current_user != 0) {
+        auto user_dir = std::filesystem::path(DUSK_ACCOUNTS) / std::to_string(globals::current_user);
+        std::filesystem::create_directories(user_dir);
+        std::filesystem::create_directory(user_dir / "modules");
+
+        if (!std::filesystem::exists(std::filesystem::path(DUSK_ACCOUNTS) / std::to_string(globals::current_user) / "config.json")) {
+            {
+                std::ofstream config_file(std::filesystem::path(DUSK_ACCOUNTS) / std::to_string(globals::current_user) / "config.json");
+                if (!config_file.is_open()) {
+                    spdlog::error("{}: Failed to create config file for user {}",
+                        FUNCSIG, globals::current_user);
+                }
+
+                config_file << fmt::format("{{\n\t\"id\": {}\n}}",
+                    globals::current_user) << std::endl;
+            }
+            spdlog::info("{}: Config for user {} has been created",
+                FUNCSIG, globals::current_user);
+        }
+    }
+}

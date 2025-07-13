43287317a13d1a49541584f6a1bca822810715b7
username000101 <cxx.username000101@gmail.com>
2025-07-13 16:31:40 +0300

    Fix incorrect operator

--- Args/src/RemoveUser.cpp
+++ Args/src/RemoveUser.cpp
@@ -8,7 +8,7 @@
 
 void args::callbacks::remove_user(const std::int64_t& user_chat_id) {
     std::filesystem::path full_user_data_path = std::filesystem::path(DUSK_ACCOUNTS) / std::to_string(user_chat_id);
-    if (auto diriter = std::filesystem::directory_iterator(full_user_data_path); diriter == std::filesystem::directory_iterator{}) {
+    if (auto diriter = std::filesystem::directory_iterator(full_user_data_path); diriter != std::filesystem::directory_iterator{}) {
         if (confirm(R"(Are you sure want to delete user ")" + std::to_string(user_chat_id) + R"(")")) {
             spdlog::warn(R"(Deleting user "{}")",
                          user_chat_id);

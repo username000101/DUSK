bf38a0beb05866ba217046839169c336df692671
username000101 <cxx.username000101@gmail.com>
2025-07-14 01:09:35 +0300

    Add the rpc_server global variable

--- Utils/Globals.hpp
+++ Utils/Globals.hpp
@@ -2,9 +2,12 @@
 
 #include <cstdint>
 
+#include <rpc/server.h>
+
 #include "Configuration.hpp"
 
 namespace globals {
     inline std::int64_t current_user;
     inline std::shared_ptr<config::Configuration> configuration;
+    inline std::shared_ptr<rpc::server> rpc_server;
 }

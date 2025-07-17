d1a329ac2359117f846861fbcb66833b9d04a39d
username000101 <cxx.username000101@gmail.com>
2025-07-17 20:34:46 +0300

    1. Some changes in log messages formatting 2. Let's declare prefix in module configuration

--- Filesystem/src/LoadModules.cpp
+++ Filesystem/src/LoadModules.cpp
@@ -31,21 +31,21 @@ std::optional<config::Module> load_module_info(const nlohmann::json& module_info
     module_obj.id = module_info_json.at("id").template get<std::string>();
 
     if (!module_info_json.contains("author")) {
-        spdlog::warn("{}: Incomplete module config(!module.contains(\"author\")); THIS IS NOT AN ERROR default value is \"UNKNOWN\"",
+        spdlog::warn(R"({}: Incomplete module config(!module.contains("author")); THIS IS NOT AN ERROR default value is "UNKNOWN")",
                      FUNCSIG);
         module_obj.author = "UNKNOWN";
     } else
         module_obj.author = module_info_json.at("author").template get<std::string>();
 
     if (!module_info_json.contains("version")) {
-        spdlog::warn("{}: Incomplete module config(!module.contains(\"version\")); THIS IS NOT AN ERROR default value is \"0.0.0\"",
+        spdlog::warn(R"({}: Incomplete module config(!module.contains("version")); THIS IS NOT AN ERROR default value is "0.0.0")",
             FUNCSIG);
         module_obj.version = "0.0.0";
     } else
         module_obj.version = module_info_json.at("version").template get<std::string>();
 
     if (!module_info_json.contains("description")) {
-        spdlog::warn("{}: Incomplete module config(!module.contains(\"description\")); THIS IS NOT AN ERROR default value is \"\"",
+        spdlog::warn(R"({}: Incomplete module config(!module.contains("description")); THIS IS NOT AN ERROR default value is "")",
             FUNCSIG);
         module_obj.description = "";
     } else
@@ -54,18 +54,9 @@ std::optional<config::Module> load_module_info(const nlohmann::json& module_info
     return module_obj;
 }
 
-const static std::unordered_map<std::string, std::string> prefix_table = {
-    {".py", "python"},
-    {".php", "php"},
-    {".ruby", "ruby"}
-};
-
 void config::UserConfiguration::load_modules() {
     for (const auto& module : this->modules_base_) {
-        std::string prefix = "";
-        if (prefix_table.contains(module.file.extension().string()))
-            prefix = prefix_table.at(module.file.extension().string());
-        globals::detached_processes.push_back(subprocess::RunBuilder({prefix, module.file.string()}).popen());
+        globals::detached_processes.push_back(subprocess::RunBuilder({module.prefix, module.file.string()}).popen());
         std::this_thread::sleep_for(std::chrono::milliseconds(200));
 
         std::shared_ptr<rpc::client> module_rpc_test;

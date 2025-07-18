643deebb60d6a48d741ad15fa1158df93d7276ee
username000101 <cxx.username000101@gmail.com>
2025-07-18 09:13:09 +0300

    Add new server commands

---
+++ Server/RPC/include/CommandSendTextMessage.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+#include <td/telegram/td_api.h>
+
+#include "Updates.hpp"
+
+inline std::pair<std::string, std::string> dusk_rpc_server_command_send_text_message(std::int64_t chat_id, const std::string& text, const std::int64_t reply_to_msg_id) {
+    auto send_msg_obj = td::td_api::make_object<td::td_api::sendMessage>();
+    send_msg_obj->chat_id_ = chat_id;
+
+    auto msg_content_obj = td::td_api::make_object<td::td_api::inputMessageText>();
+    auto formatted_text_obj = td::td_api::make_object<td::td_api::formattedText>();
+    formatted_text_obj->text_ = text;
+    msg_content_obj->text_ = std::move(formatted_text_obj);
+
+    send_msg_obj->input_message_content_ = std::move(msg_content_obj);
+
+    if (reply_to_msg_id != 0) {
+        auto msg_reply_to_obj = td::td_api::make_object<td::td_api::inputMessageReplyToMessage>();
+        msg_reply_to_obj->message_id_ = reply_to_msg_id;
+        send_msg_obj->reply_to_ = std::move(msg_reply_to_obj);
+    }
+
+    auto result_request_str = td::td_api::to_string(send_msg_obj);
+    auto response = update::send_request(std::move(send_msg_obj));
+    if (!response.object)
+        return std::make_pair(result_request_str, "");
+    return std::make_pair(result_request_str, td::td_api::to_string(response.object));
+
+
+}
---
+++ Server/RPC/include/CommandsEventsHandling.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <algorithm>
+#include <cstdint>
+#include <string>
+#include <unordered_map>
+
+#include <rpc/client.h>
+#include <td/telegram/td_api.h>
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+
+#include "Events.hpp"
+
+static inline std::unordered_map<std::int64_t, std::vector<std::pair<std::uint16_t, std::string>>> external_handlers;
+
+
+inline bool process_update(events::ModifiedEventSignature update) {
+    static auto logger = std::make_shared<spdlog::logger>("RPC::process_update", spdlog::sinks_init_list{std::make_shared<spdlog::sinks::stdout_color_sink_mt>()});
+    if (external_handlers.contains(update->get_id())) {
+        for (auto& handler : external_handlers.at(update->get_id())) {
+            rpc::client client("127.0.0.1", handler.first);
+            try {
+                client.async_call(handler.second, td::td_api::to_string(*update));
+            } catch (std::exception& rpcerr) {
+                logger->error("rpc::client throw an exception: {}",
+                    rpcerr.what());
+            }
+        }
+    }
+    return true;
+}
+
+inline std::string dusk_rpc_server_command_set_event_handler(std::int64_t event_id, const std::string& handler_function_name, std::uint16_t port) {
+    static auto logger = std::make_shared<spdlog::logger>("RPC::set_event_handler", spdlog::sinks_init_list{std::make_shared<spdlog::sinks::stdout_color_sink_mt>()});
+
+    if (external_handlers.contains(event_id)) {
+        bool has = false;
+        std::ranges::for_each(external_handlers.at(event_id), [&has, &handler_function_name](auto& handler) { if (handler.second == handler_function_name) has = true; });
+        if (has) {
+            logger->warn("Attempt to overwrite the handler for event {} and name {}",
+                event_id, handler_function_name);
+            return "already_exists";
+        }
+    }
+    external_handlers[event_id].push_back(std::make_pair(port, handler_function_name));
+    logger->info("Setting handler for event {} with name {} from port {}",
+    event_id, handler_function_name, port);
+    return "";
+}
+
+inline std::string dusk_rpc_server_unset_event_handler(std::int64_t event_id, const std::string& handler_function_name) {
+    if (external_handlers.contains(event_id)) {
+        auto& handlers = external_handlers.at(event_id);
+        for (auto iter = handlers.begin(); iter != handlers.end(); ++iter) {
+            if (iter->second == handler_function_name) {
+                handlers.erase(iter);
+                break;
+            }
+
+        }
+    }
+}

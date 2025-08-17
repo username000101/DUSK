7fcb0e200a84369766ca8182fee6aa47e675f656
username000101 <cxx.username000101@gmail.com>
2025-08-17 21:32:04 +0300

    Remove file

--- Server/RPC/include/CommandInstallModpack.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "InstallModpack.hpp"
-
-#include <unordered_map>
-
-inline bool dusk_rpc_server_command_install_modpack(const std::string& modpack_path) {
-    return modpack::install_modpack<std::unordered_map>(modpack_path);
-}

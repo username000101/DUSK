13f23155bccc1afc7c2d1a1ccf1d3b7535c3d4d1
username000101 <cxx.username000101@gmail.com>
2025-08-31 07:21:41 +0300

    Finally working prototype of the check_access function

--- Server/RPC/include/PrivilegedRequest.hpp
+++ Server/RPC/include/PrivilegedRequest.hpp
@@ -1,3 +1,25 @@
 #pragma once
 
-#include "InitAccessTokens.hpp"
+#include <unordered_map>
+#include <string>
+
+extern const std::unordered_map<std::string, std::pair<std::string, std::string>>& get_access_tokens();
+
+inline bool check_access(const std::string& access_token, const std::string& request) {
+    static auto logger = std::make_shared<spdlog::logger>("RPC Access Manager", spdlog::sinks_init_list{std::make_shared<spdlog::sinks::stdout_color_sink_mt>()});
+    spdlog::initialize_logger(logger);
+
+    auto& access_tokens = get_access_tokens();
+    if (access_tokens.contains(access_token)) {
+        auto& pair = access_tokens.at(access_token);
+        logger->info("Call '{}' from '{}'",
+            request, pair.first);
+        logger->debug("Additional debug info: '{}' is a '{}'",
+            pair.first, pair.second);
+        return true;
+    } else {
+        logger->warn("Attempt to call '{}' from 'unknown' because token '{}' is invalid",
+            request, access_token);
+        return false;
+    }
+}

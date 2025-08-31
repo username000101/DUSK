1a609dbfec222e09ef571f818556e6bb739216ab
username000101 <cxx.username000101@gmail.com>
2025-08-31 07:20:42 +0300

    Finally working prototype of the init_access_tokens function

--- Server/RPC/include/InitAccessTokens.hpp
+++ Server/RPC/include/InitAccessTokens.hpp
@@ -20,9 +20,11 @@
 
 static inline std::unordered_map<std::string, std::pair<std::string, std::string>> access_tokens;
 
-inline const std::unordered_map<std::string, std::string>& get_access_tokens() { return access_tokens; }
+inline const std::unordered_map<std::string, std::pair<std::string, std::string>>& get_access_tokens() { return access_tokens; }
 
 inline void init_access_tokens(const std::vector<config::BaseModuleInfo>& el_modules) {
+    static auto logger = std::make_shared<spdlog::logger>("DML", spdlog::sinks_init_list{ std::make_shared<spdlog::sinks::stdout_color_sink_mt>() });
+    spdlog::initialize_logger(logger);
 
     std::optional<config::BaseModuleInfo> main_module = std::nullopt;
     for (auto& el_module : el_modules) {
@@ -60,12 +62,12 @@ inline void init_access_tokens(const std::vector<config::BaseModuleInfo>& el_mod
                 el_module.file.string(), convert_rpc_error(rpcerr)));
         }
 
-        access_tokens[generated_access_token] = std::make_pair(el_module.el_module.file.string();
+        access_tokens[generated_access_token] = std::make_pair(el_module.mod_id, el_module.file.string());
     }
 
     if (main_module) {
         auto value = main_module.value();
-        spdlog::info("{}: Loading the main module '{}'",
+        logger->info("Loading the main module '{}'",
             value.file.string());
         server::rpc::up_rpc_server();
 
@@ -74,6 +76,15 @@ inline void init_access_tokens(const std::vector<config::BaseModuleInfo>& el_mod
             main_module_process = subprocess::Popen({value.file.string()}, {});
         else
             main_module_process = subprocess::Popen({value.prefix, value.file.string()}, {});
+
+        rpc::client main_module_rpc("127.0.0.1", value.rpc_port);
+        auto generated_access_token = gen_access_token();
+        access_tokens[generated_access_token] = std::make_pair(value.mod_id, value.file.string());
+        try {
+            main_module_rpc.call(value.set_access_token_function, generated_access_token);
+        } catch (rpc::rpc_error& rpcerr) {
+            shutdown(EXIT_FAILURE, std::format("The main module throws an exception: {}",convert_rpc_error(rpcerr)));
+        }
 
         while (!main_module_process.poll()) { std::this_thread::sleep_for(std::chrono::milliseconds(100)); }
 
